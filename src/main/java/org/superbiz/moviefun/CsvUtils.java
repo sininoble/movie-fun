fb5e2ed0909743508fb74230a15f6eaaed715f65
SInimole Chacko <SInimole.Chacko@libertymutual.com>
2020-04-20 09:28:57 -0400

    Read from filesystem

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -13,8 +13,10 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+       // try {
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+            Scanner scanner = new Scanner(classLoader.getResourceAsStream(path)).useDelimiter("\\A");
+            //Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
@@ -22,9 +24,7 @@ public class CsvUtils {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        //}
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
